7f89a4987a4d1ecf0094b4a8a372a391a2efe52b
Hasban Fardani <hasbanfardani77@gmail.com>
2026-06-22 12:52:48 +0700

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.js
@@ -0,0 +1,34 @@
+class MyHashSet {
+    constructor() {
+        this.arr = [];
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    add(key) {
+        this.arr.push(key)
+        // console.log('[ADD] key : ', key, 'arr', this.arr)
+    }
+
+    /**
+     * @param {number} key
+     * @return {void}
+     */
+    remove(key) {
+        this.arr = this.arr.filter(v => v != key)
+        // console.log('[REMOVE] key : ', key, 'arr', this.arr)
+    }
+
+    /**
+     * @param {number} key
+     * @return {boolean}
+     */
+    contains(key) {
+        let c = this.arr.includes(key)
+        // console.log('[CONTAINS] key : ', key, 'arr', this.arr, 'contain',c)
+        return c
+    }
+}
+
